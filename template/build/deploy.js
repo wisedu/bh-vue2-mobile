3699ad910ad77d4f7f5cd015ce265285112de6e4
zhuhui <huizhu@wisedu.com>
2017-05-09 15:52:10 +0800

    deploy 修改打包目录结构为emap目录结构

--- template/build/deploy.js
+++ template/build/deploy.js
@@ -1,12 +1,16 @@
 'use strict';
-
 require('shelljs/global');
 
 let packageName = require('../package.json').packageName;
+exec('chmod u+x ./build/deploy.sh')
+exec(`./build/deploy.sh ${packageName}`)
 
+
+/* 旧版本
 mkdir(packageName);
 cp('-R', './dist', packageName);
 cp('-R', './static', packageName);
 cp('index.html', packageName);
 exec(`zip -r ${packageName}.zip ${packageName}`);
 rm('-rf', `${packageName}`)
+*/
---
+++ template/build/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+echo '---- deploy start ----'
+packageName=$1
+mkdir $packageName
+touch ${packageName}/app_info.xml
+touch ${packageName}/EMAP_APP
+mkdir ${packageName}/classes
+mkdir ${packageName}/lib
+mkdir ${packageName}/web
+cp -R ./dist ${packageName}/web
+cp -R ./static ${packageName}/web
+cp ./index.html ${packageName}/web
+
+#判断根目录下的app_info.xml是否存在，若存在就放入打包中
+if [ -f './app_info.xml' ]
+then 
+cp ./app_info.xml ${packageName}
+fi
+
+zip -r ${packageName}.zip ${packageName}
+rm -rf ${packageName}
+
+if [ ! -f './app_info.xml' ]
+then
+echo -e "\033[33m warning: 根目录中不存在app_info.xml！ \033[0m"
+fi
+echo '---- deploy complete ----'
