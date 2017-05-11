f6cf6b1796de3dbab36dfdb07e25abbdd133106b
zhuhui <huizhu@wisedu.com>
2017-05-11 15:36:13 +0800

    打包优化

--- template/build/deploy.bat
+++ template/build/deploy.bat
@@ -11,13 +11,12 @@ md %packageName%"\lib"
 md %packageName%"\web"
 md %packageName%"\web\dist"
 md %packageName%"\web\static"
-echo nul>%packageName%"\EMAP_APP"
 XCOPY ".\dist" %packageName%"\web\dist" /e
 XCOPY ".\static" %packageName%"\web\static" /e
 copy ".\index.html" %packageName%"\web"
 
-if exist "app_info.xml" (
-	copy "app_info.xml" %packageName%
-) else echo "warning: 根目录中不存在app_info.xml!"
+REM if exist "app_info.xml" (
+REM 	copy "app_info.xml" %packageName%
+REM ) else echo "warning: 根目录中不存在app_info.xml!"
 
 echo "deploy complete !"
