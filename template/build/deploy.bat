45efaa155384bcbdf4764a1dee6786a0defd55f1
zhuhui <huizhu@wisedu.com>
2017-05-26 13:55:04 +0800

    bat打包优化

--- template/build/deploy.bat
+++ template/build/deploy.bat
@@ -1,28 +1,29 @@
 @ECHO OFF
-set packageName=%1
-
+set name=%1
 echo "---- deploy start ----"
-
-if exist %packageName% (
-	rd %packageName% /s /q
+echo "---- delete file start ----"
+if exist "%name%" (
+    rd "%name%" /s /q
 )
-
+echo "---- delete file complete ----"
+echo "---- add file start ----"
+md "%name%"
+md "%name%\classes"
+md "%name%\lib"
+md "%name%\web"
+md "%name%\web\dist"
+md "%name%\web\static"
+XCOPY ".\dist" "%name%\web\dist" /e
+XCOPY ".\static" "%name%\web\static" /e
+copy ".\index.html" "%name%\web"
+echo "---- add file complete ----"
 if exist "app_info.xml" (
-	copy "app_info.xml" %packageName%
+    echo "---- app_info has ----"
+    copy "app_info.xml" "%name%"
+    echo "---- app_info copyed ----"
 ) else (
-	echo "error：There is a missing app_info.xml in the root directory! Please put app_info.xml in the root directory and then execute the package"
-	exit
+    echo "error:there is a missing app_info.xml in the root directory! please put app_info.xml in the root directory and then execute the package"
+    rd "%name%" /s /q
+    exit
 )
-
-md %packageName%
-md %packageName%"\classes"
-md %packageName%"\lib"
-md %packageName%"\web"
-md %packageName%"\web\dist"
-md %packageName%"\web\static"
-XCOPY ".\dist" %packageName%"\web\dist" /e
-XCOPY ".\static" %packageName%"\web\static" /e
-copy ".\index.html" %packageName%"\web"
-
-
 echo "---- deploy complete ----"
