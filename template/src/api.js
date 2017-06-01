a7df6f7b083123da8ae94ae52a608fc20a17bc1f
Elvis <shuaiqy@hotmail.com>
2017-06-01 11:53:25 +0800

    Create api.js

---
+++ template/src/api.js
@@ -0,0 +1,10 @@
+
+export default {
+    weeklist: global.HOST + '/sys/mykbxt/api/getMyTimeTableList.do',
+    daylist: global.HOST + '/sys/mykbxt/api/queryDayAndFutureCourses.do',
+    week: global.HOST + '/sys/mykbxt/api/getSchoolCalendar.do',
+    memberList: global.HOST + '/sys/mykbxt/api/queryCourseMembers.do',
+    geterr: global.HOST + '/sys/mykbxt/api/queryErrorMessages.do',
+    err: global.HOST + '/sys/mykbxt/api/feedBackInfo.do',
+    userInfo: global.HOST + '/sys/itservicecommon/api/queryAppAndUserDatas.do'
+}
