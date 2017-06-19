c09d1c09d4f80a88a02e3d143466950dedaf6a1e
Elvis <shuaiqy@hotmail.com>
2017-06-19 17:24:13 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -14,20 +14,50 @@ Vue.use(VueRouter);
 // Vue.use(VueResource);
 // Vue.http.options.credentials = true;
 const router = new VueRouter(route);
-let Init = () => {
-    console.log("Init!!!");
-    new Vue({
-            el: '#app',
-            router: router,
-            render: h => h(App)
-    });
+
+global.SDK = null;
+
+
+function getSDKConfig () {
+  return new Promise((resolve, reject) => {
+    let config = {
+      wx: {
+        uploadImgsToEmapUrl: WEBPACK_CONIFG_HOST + '/sys/yxapp/WechatServiceStu/saveFileFromWechat.do' //TODO: 上传接口
+      }, //微信jdk初始化参数
+      dd: {}, //钉钉jdk初始化参数
+    }
+    if (/micromessenger/.test(navigator.userAgent.toLowerCase())) {
+      config.wx.url = WEBPACK_CONIFG_HOST + '/sys/yxapp/WechatServiceStu/getWechatSign.do' //TODO: 微信签名服务
+      getWechatSign(window.location.href.replace(/#(\S+)?/, '')).then(({data: resp}) => {
+        if (resp.status == 200) {
+          let signData = resp.datas
+          signData.corpId = signData.corpid
+          signData.nonceStr = signData.noncestr
+          config.wx.signData = signData
+          resolve(config)
+        } else {
+          ons.notification.alert('微信jsdk初始化失败')
+          reject(resp)
+        }
+      }, () => {
+        ons.notification.alert('微信jsdk初始化失败')
+        reject()
+      })
+    } else {
+      setTimeout(function() {
+        resolve(config)
+      }, 0)
+    }
+
+    return config
+  })
 }
 
-init(() => {
-    console.log("init");
-    var sdk = SDK();
-    if(sdk.UI && sdk.UI.toggleNavBar) {
-        sdk.UI.toggleNavBar(false);
+getSDKConfig().then((config) => {
+  init((res) => {
+    if (res.type === 'success') {
+      global.SDK = res.sdk
+      router.start(App, '#app')
     }
-    Init()
+  }, config)
 })
