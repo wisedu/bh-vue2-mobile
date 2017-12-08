4fe762d2461df12b8a2f89413b4c6a12b1b40826
Elvis <shuaiqy@hotmail.com>
2017-12-08 10:05:56 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -1,60 +1,18 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
-import Mint from 'bh-mint-ui2';
-import { Toast } from 'bh-mint-ui2';
 import route from './router';
 import * as utils from './utils'
 import api from './api'
-import axios from 'axios'
-import 'bh-mint-ui2/lib/style.css'
-{{#useNativeSDK}}
-import init from 'bh-mixin-sdk'
-{{/useNativeSDK}}
 
-if (window.smile) {
-  window.Vue = Vue
-  window.axios = axios
-  if (WEBPACK_CONIFG_HOST) {
-      window.WEBPACK_CONIFG_HOST = WEBPACK_CONIFG_HOST
-  }
-  // 初始化eventBus
-  window.smile.eventBus = new Vue()
+let Vue = window.Vue;
+let VueRouter = window.VueRouter;
+let axios = axios;
+let Toast = window.$toast;
+
+if (WEBPACK_CONIFG_HOST) {
+    window.WEBPACK_CONIFG_HOST = WEBPACK_CONIFG_HOST
 }
 
-Vue.use(Mint);
-Vue.use(VueRouter);
 const router = new VueRouter(route);
-
-router.beforeEach((to, from, next) => {
-  // 加载页面smile依赖
-  if (window.smile) {
-      var require = []
-      var promiseArr = []
-      // 拉取require配置中的表单
-      if (to.meta.require && to.meta.require.length > 0) {
-          require = require.concat(to.meta.require)
-      }
-      // 拉取传参中的表单
-      if (to.query.smileForm) {
-          require.push(to.query.smileForm)
-      }
-      require.map(function(item) {
-          if (!window.smile.components[item]) {
-              promiseArr.push(window.smile.loadPage(item))
-          }
-      })
-      if (promiseArr.length > 0) {
-          Promise.all(promiseArr).then(function() {
-              next()
-          })
-      } else {
-          next()
-      }
-  } else {
-      next()
-  }
-})
 
 {{#useNativeSDK}}
 global.SDK = null;
@@ -113,7 +71,7 @@ function getSDKConfig() {
 }
 
 getSDKConfig().then((config) => {
-  init((res) => {
+  BH_MOBILE.default((res) => {
     if (res.type === 'success') {
       global.SDK = res.sdk;
 
