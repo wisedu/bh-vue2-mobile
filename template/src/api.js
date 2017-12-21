6c42a4142d6b21e3f87a49834d34e77eca9d8946
Elvis <shuaiqy@hotmail.com>
2017-12-21 21:50:56 +0800

    Update api.js

--- template/src/api.js
+++ template/src/api.js
@@ -6,11 +6,11 @@
 
 export default {
     getWechatSign: 'http://res.wisedu.com:9090/checkSign',
-    weeklist: WEBPACK_CONIFG_HOST + '/sys/mykbxt/api/getMyTimeTableList.do',
-    daylist: WEBPACK_CONIFG_HOST + '/sys/mykbxt/api/queryDayAndFutureCourses.do',
-    week: WEBPACK_CONIFG_HOST + '/sys/mykbxt/api/getSchoolCalendar.do',
-    memberList: WEBPACK_CONIFG_HOST + '/sys/mykbxt/api/queryCourseMembers.do',
-    geterr: WEBPACK_CONIFG_HOST + '/sys/mykbxt/api/queryErrorMessages.do',
-    err: WEBPACK_CONIFG_HOST + '/sys/mykbxt/api/feedBackInfo.do',
-    userInfo: WEBPACK_CONIFG_HOST + '/sys/itservicecommon/api/queryAppAndUserDatas.do'
+    weeklist: WEBPACK_CONFIG_HOST + '/sys/mykbxt/api/getMyTimeTableList.do',
+    daylist: WEBPACK_CONFIG_HOST + '/sys/mykbxt/api/queryDayAndFutureCourses.do',
+    week: WEBPACK_CONFIG_HOST + '/sys/mykbxt/api/getSchoolCalendar.do',
+    memberList: WEBPACK_CONFIG_HOST + '/sys/mykbxt/api/queryCourseMembers.do',
+    geterr: WEBPACK_CONFIG_HOST + '/sys/mykbxt/api/queryErrorMessages.do',
+    err: WEBPACK_CONFIG_HOST + '/sys/mykbxt/api/feedBackInfo.do',
+    userInfo: WEBPACK_CONFIG_HOST + '/sys/itservicecommon/api/queryAppAndUserDatas.do'
 }
